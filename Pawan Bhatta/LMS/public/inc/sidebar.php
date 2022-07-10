2ebaebeb16941235da79905b42173776a93d8f36
Pawan Bhatta <69564458+cyber-coders-hub@users.noreply.github.com>
2022-07-10 21:01:39 +0545

    Create sidebar.php

---
+++ Pawan Bhatta/LMS/public/inc/sidebar.php
@@ -0,0 +1,23 @@
+
+            <div class="col-sm-3 bg-dark text-light text-center">
+                <h3 class="text-primary"><a href="index.php" class="text-decoration-none text-primary"> Library Management System</a></h3>
+                <br>
+                <div class="lead"><a href="index.php" class="text-decoration-none text-success"> Add Book</a></div>
+                <hr>
+                <div class="lead text-muted "><a href="managebooks.php" class="text-decoration-none text-success">Manage Books</a></div>
+                <hr>
+                <div class="lead text-muted"><a href="issuebooks.php" class="text-decoration-none text-success">Issue Books</a></div>
+                <hr>
+                <div class="lead text-muted"><a href="manageissuedbooks.php" class="text-decoration-none text-success">Manage Issued Books</a></div>
+                <hr>
+                <div class="lead text-muted"><a href="addstudents.php" class="text-decoration-none text-success">Add Students</a></div>
+                <hr>
+                <div class="lead text-muted"><a href="managestudents.php" class="text-decoration-none text-success">Manage Students</a></div>
+                <hr>
+                <div class="lead text-muted"><a href="adduser.php" class="text-decoration-none text-success">Add User</a></div>
+                <hr>
+                <div class="lead text-muted"><a href="manageusers.php" class="text-decoration-none text-success">Manage Users</a></div>
+                <hr>
+                <button type="button" class="btn btn-danger"><a href="../../process/logout.php" class="text-decoration-none text-light">Logout</a></button>
+                <hr>
+            </div>
